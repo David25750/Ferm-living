d185dac2df3916c4ee313265b9e37978f83a2050
Eliisou <elisabraun444@gmail.com>
2022-12-05 14:07:50 +0100

    init vue js

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script>
+  import MyButton from "./../components/MyButton.vue";
+  import MyTitle from "./../components/MyTitle.vue";
+  export default {
+    components: {
+      MyButton,
+      MyTitle,
+    },
+  };
+</script>
+
+<template>
+  <main>
+    <MyTitle size="tiny" type="H2" label="Petit Titre H2"></MyTitle>
+    <MyTitle size="big" label="Grand Titre H1"></MyTitle>
+    
+    <MyButton label="Cliquez la svp" href="/lien"></MyButton>
+    <MyButton label="Valider le formulaire" type="secondary"></MyButton>
+  </main>
+</template>
