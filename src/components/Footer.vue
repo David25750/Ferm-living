623c6a39cd9e35dd19453e689cf61d54d995b908
drivera <david.rivera13.02.67@gmail.com>
2022-12-07 10:14:12 +0100

    changement en

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,44 +1,121 @@
 <script>
+export default{
+    data() {
+            return{
+
+                reseaux: [
+                    {
+                        id: 1,
+                        desc: "logo facebook",
+                        label: "/src/assets/icon/facebook.svg",
+                    },
+                    {
+                        id: 2,
+                        desc: "logo pinterest",
+                        label: "/src/assets/icon/pinterest.svg",
+                    },
+                    {
+                        id: 3,
+                        desc: "logo instagram",
+                        label: "/src/assets/icon/insta.svg",
+                    },
+                    {
+                        id: 4,
+                        desc: "logo youtube",
+                        label: "/src/assets/icon/youtube.svg",
+                    },
+
+                ],
+
+                menu: [
+                    [
+                        {
+                            id: 1,
+                            lien: "/legal-notice",
+                            label: "Legal Notice",
+                            isTitle: true,
+                        },
+                        {
+                            id: 2,
+                            lien: "",
+                            label: "CVG",
+                            isTitle: false,
+                        },
+                        {
+                            id: 3,
+                            lien: "",
+                            label: "RGPD",
+                            isTitle: false,
+                        },
+                        {
+                            id: 4,
+                            lien: "",
+                            label: "Cookies",
+                            isTitle: false,
+                        },
+                        {
+                            id: 4,
+                            lien: "",
+                            label: "Privacy",
+                            isTitle: false,
+                        },
+                    ],
+                    [
+                        {
+                            id: 1,
+                            lien: "/legal-notice",
+                            label: "Navigation",
+                            isTitle: true,
+                        },
+                        {
+                            id: 2,
+                            lien: "",
+                            label: "Home",
+                            isTitle: false,
+                        },
+                        {
+                            id: 3,
+                            lien: "",
+                            label: "Our Universe",
+                            isTitle: false,
+                        },
+                        {
+                            id: 4,
+                            lien: "",
+                            label: "Our Responsability",
+                            isTitle: false,
+                        },
+                        {
+                            id: 5,
+                            lien: "",
+                            label: "Catalog",
+                            isTitle: false,
+                        },
+                        {
+                            id: 6,
+                            lien: "",
+                            label: "Heart Finding",
+                            isTitle: false,
+                        },
+                    ]
+                ]
+            }
+        }
+}
+
+
 </script>
 <template>
     <footer class="footer">
         <div class="footer__haut">
-            <ul class="footer__list -un">
-                <li class="footer__item">
-                    <h2>Legal Notice</h2>
-                </li>
-                <li class="footer__item">
-                    <a href="" class="footer__link">RGPD</a>
-                </li>
-                <li class="footer__item">
-                    <a href="" class="footer__link">Cookies</a>
-                </li>
-                <li class="footer__item">
-                    <a href="" class="footer__link">Privacy</a>
-                </li>
-            </ul>
+            <ul v-for="(elements, index) in menu" :key="index" :class="`footer__list -index${index + 1}`">
+                <li v-for="element in elements" :key="element.id"  class="footer__item">
+                    <h2 v-if="element.isTitle === true">{{ element.label }}</h2>
+                    <a v-else :href="element.lien"  class="footer__link"> {{ element.label }} </a>
 
-            <ul class="footer__list -deux">
-                <li class="footer__item">
-                    <h2>Navigation</h2>
-                </li>
-                <li class="footer__item">
-                    <a href="" class="footer__link">Home</a>
-                </li>
-                <li class="footer__item">
-                    <a href="" class="footer__link">Our Universe</a>
-                </li>
-                <li class="footer__item">
-                    <a href="" class="footer__link">Our Responsabilities</a>
-                </li>
-                <li class="footer__item">
-                    <a href="" class="footer__link">Catalog</a>
-                </li>
-                <li class="footer__item">
-                    <a href="" class="footer__link">Heart Finding</a>
                 </li>
             </ul>
-            <ul class="footer__list -trois">
+            <ul class="footer__list -index3">
                 <li class="footer__item -logo">
                     <img src="../assets/img/logo_ferm_living.svg" alt="">
                 </li>
@@ -47,17 +124,8 @@
         <div class="footer__bas">
             <p>Copyright FermLiving 2022</p>
             <ul class="footer__reseaux">
-                <li class="footer__logo">
-                    <img src="../assets/icon/facebook.svg" alt="logo facebook">
-                </li>
-                <li class="footer__logo">
-                    <img src="../assets/icon/pinterest.svg" alt="logo pinterest">
-                </li>
-                <li class="footer__logo">
-                    <img src="../assets/icon/insta.svg" alt="logo instagram">
-                </li>
-                <li class="footer__logo">
-                    <img src="../assets/icon/youtube.svg" alt="logo youtube">
+                <li v-for="reseau in reseaux" :key="reseau.id" class="footer__logo">
+                    <img :src="reseau.label" :alt="reseau.desc">
                 </li>
             </ul>
         </div>
