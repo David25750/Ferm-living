377df304341aec92b173b41cb3fc0d95fe4cd285
Davyy <spartiate25750@gmail.com>
2023-01-11 10:06:53 +0100

    creation de props pour bouton + creation de page produit

---
+++ src/components/Product.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      product: {}
+    }
+  },
+  created() {
+    // Récupérer les informations du produit en fonction de l'ID
+    axios.get('/api/products/' + this.$route.params.id).then(response => {
+      this.product = response.data
+    })
+  }
+}
+</script>
+<template>
+    <div>
+      <h1>{{ product.name }}</h1>
+      <p>{{ product.price }}</p>
+      <p>{{ product.description }}</p>
+      <img :src="product.image" :alt="product.name">
+    </div>
+  </template>
+<style lang="scss" scoped>
+
+ 
+</style>
--- src/views/Universe.vue
+++ src/views/Universe.vue
@@ -1,9 +1,30 @@
+<script>
+import Button1 from "../components/elements/Button.vue";
+
+export default {
+    components: {
+      Button1
+    },
+  };
+</script>
 <template>
+
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <button1  
+            :color="'grey'">
+            
+            Réservation en magasin
+  </Button1>
+  <button1  :color="'brown'" >
+            Voir le panier
+  </Button1>
+  <button1  :color="'white'" >
+            click
+  </Button1>
 </template>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
