ef58cd19750a07585a27d24f39572c00d7641b2e
nathanbroglin <nathan.broglin@gmail.com>
2023-01-12 08:41:46 +0100

    création html catalogSofa

---
+++ src/views/catalogue/CatalogSofa.vue
@@ -0,0 +1,81 @@
+<script>
+
+</script>
+
+<template>
+    <!--Compartiment du header a mettre ici !-->
+
+    <!--Compartiment du navbar catalogue a mettre ici !-->
+
+    <div class="herosofa">
+        /ici les modales/
+
+
+    </div>
+    <div class="sofadesc">
+        <h3 class="sofadesc__title">Sofa</h3>
+        <p class="sofadesc__text">What makes a home? We believe it’s the feeling of belonging. The living room can be
+            characterised as the heart of the home – a place to unwind and focus on the important things in life. The
+            items you choose to surround yourself with should be a reflection of this, allowing you to always feel
+            comfortably you.</p>
+    </div>
+    <div class="sofasave">
+        <p class="sofasave__date">1 au 31 octobre 2022</p>
+        <h3 class="sofasave__info">Save 20% on all sofas</h3>
+    </div>
+    <div class="threesofas">
+        <div class="threesofas__catena">
+            <img class="threesofas__catenaimg" src="#" alt="#">
+            <div class="threesofas__catenacontent">
+                <h3 class="threesofas__catenatitle">Catena</h3>
+                <p class="threesofas__catenadesc">Discover our Catena sofas, customizable sofas for all interiors,
+                    available in many colors at variable prices depending on the modules chosen</p>
+            </div>
+            <button class="treesofas__button">
+                <a href="#">See the Product</a>
+            </button>
+
+        </div>
+        <div class="threesofas__turn">
+
+            <div class="threesofas__turncontent">
+                <h3 class="threesofas__turntitle">Turn</h3>
+                <p class="threesofas__turndesc">Discover our Turn sofas, customizable sofas for all interiors, available
+                    in many colors at variable prices depending on the modules chosen
+
+                </p>
+                <button class="treesofas__button">
+                    <a href="#">See the Product</a>
+                </button>
+
+            </div>
+            <img class="threesofas__turnaimg" src="#" alt="#">
+
+        </div>
+        <div class="threesofas__rico">
+            <img class="threesofas__ricoimg" src="#" alt="#">
+            <div class="threesofas__ricocontent">
+                <h3 class="threesofas__ricotitle">Rico</h3>
+                <p class="threesofas__ricodesc">Discover our Catena sofas, customizable sofas for all interiors,
+                    available in many colors at variable prices depending on the modules chosen
+                </p>
+                <button class="treesofas__button">
+                    <a href="#">See the Product</a>
+                </button>
+
+            </div>
+
+        </div>
+    </div>
+
+    <!--Compartiment de get inspired a mettre ici !-->
+
+
+    <!--Compartiment des valeurs a mettre ici !-->
+
+    <!--Compartiment du footer a mettre ici !-->
+</template>
+
+<style lang="scss" scoped>
+
+</style>
